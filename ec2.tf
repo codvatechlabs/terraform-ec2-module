1a29fdac884d3c47e8c2a22cb9c133eedd233bb7
codvatechlabs <78929919+codvatechlabs@users.noreply.github.com>
2022-03-24 21:29:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "web" {
   count         = "2"
   ami           = "ami-04893cdb768d0f9ee"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   subnet_id     = "subnet-6f19ee04"
   key_name      = "devops2022"
   tags = {
